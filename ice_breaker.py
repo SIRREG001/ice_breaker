05d68c4b8e131c170ab9eab212a07e3f8b096845
SIRREG001 <udochukwureginald78@gmail.com>
2024-07-05 16:04:52 +0100

    set up flaskapp

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,16 +1,17 @@
 from dotenv import load_dotenv
 import os
 from langchain.prompts.prompt import PromptTemplate
-from langchain.chains import LLMChain
 from langchain_openai import ChatOpenAI
+from output_parsers import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linked_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.twitter import scrape_user_tweets
 from output_parsers import summary_parser
+from typing import Tuple
 
 
-def ice_breaker_with(name: str) -> str:
+def ice_breaker_with(name: str) -> Tuple[Summary, str]:
     linkedin_url = linked_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url, mock=True)
 
@@ -35,9 +36,9 @@ def ice_breaker_with(name: str) -> str:
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     chain = summary_prompt_template | llm | summary_parser
-    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
+    res: Summary = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == '__main__':
